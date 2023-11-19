53eedc2c7de13fb3d83951aa2cba8851cc3ae6ad
eabdikhalyk <73381587+eabdikhalyk@users.noreply.github.com>
2023-11-19 16:03:52 +0600

    Update SigningUpForm.java

--- src/test/java/pages/SigningUpForm.java
+++ src/test/java/pages/SigningUpForm.java
@@ -1,10 +1,10 @@
 package pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SigningUpForm {
     WebDriver driver;
+    
     By firstName = By.id("customer.firstName");
     By lastName = By.id("customer.lastName");
     By address = By.id("customer.address.street");
@@ -18,6 +18,7 @@ public class SigningUpForm {
     By buttonRegister = By.cssSelector("#customerForm input.button");
     By confirmPassword = By.id("repeatedPassword");
     By title = By.className("title");
+    
     By messageOfRegistration = By.cssSelector("#rightPanel p");
     public SigningUpForm(WebDriver driver){
         this.driver = driver;
@@ -26,37 +27,48 @@ public class SigningUpForm {
     public void enterFirstName(String text){
         driver.findElement(firstName).sendKeys(text);
     }
+    
     public void enterLastName(String text){
         driver.findElement(lastName).sendKeys(text);
     }
+    
     public void enterAddress(String text){
         driver.findElement(address).sendKeys(text);
     }
+    
     public void enterCity(String text){
         driver.findElement(city).sendKeys(text);
     }
+    
     public void enterState(String text){
         driver.findElement(state).sendKeys(text);
     }
+    
     public void enterZipCode(String text){
         driver.findElement(zipCode).sendKeys(text);
     }
+    
     public void enterSSN(String text){
         driver.findElement(ssn).sendKeys(text);
     }
+    
     public void enterUserName(String text){
         driver.findElement(username).sendKeys(text);
     }
+    
     public void enterPassword(String text){
         driver.findElement(password).sendKeys(text);
         driver.findElement(confirmPassword ).sendKeys(text);
     }
+    
     public void clickToRegister(){
         driver.findElement(buttonRegister).click();
     }
+    
     public String getTitleOfForm(){
         return driver.findElement(title).getText();
     }
+    
     public String getMessageOfRegistration(){
         return driver.findElement(messageOfRegistration).getText();
     }
